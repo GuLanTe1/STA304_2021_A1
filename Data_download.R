015c02c248513c4661c5afa4c22196ea3a5fb07d
GuLanTe1 <wxy1215711402@gmail.com>
2022-02-03 22:18:06 -0500

    to data

---
+++ Data_download.R
@@ -0,0 +1,10 @@
+### Preamble ###
+# Purpose: Download data from opendatatoronto
+# Author: Xiangyu Wang 
+# Date: 03 Feb 2022
+# Contact: xiangyu.wang@mail.utoronto.ca
+# License: MIT
+
+#### Workspace setup ####
+library(tidyverse)
+library(opendatatoronto)
