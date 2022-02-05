c4c2df641f7001912c3bb869c1942c0000335941
GuLanTe1 <71858148+GuLanTe1@users.noreply.github.com>
2022-02-05 16:51:02 -0500

    Add files via upload

--- Data_download.R
+++ Data_download.R
@@ -3,8 +3,22 @@
 # Author: Xiangyu Wang 
 # Date: 03 Feb 2022
 # Contact: xiangyu.wang@mail.utoronto.ca
-# License: MIT
+# License: Open Government Licence – Toronto
 
 #### Workspace setup ####
-library(tidyverse)
 library(opendatatoronto)
+library(dplyr)
+
+# get package
+package <- show_package("cba07a90-984b-42d2-9131-701c8c2a9788")
+package
+
+# get all resources for this package
+resources <- list_package_resources("cba07a90-984b-42d2-9131-701c8c2a9788")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+death_registry <- filter(datastore_resources, row_number()==1) %>% get_resource()
+death_registry
