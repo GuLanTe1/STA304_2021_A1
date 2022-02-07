3008c3709cdcfa0276e6e9b1a5e4c8ba8588add4
GuLanTe1 <71858148+GuLanTe1@users.noreply.github.com>
2022-02-06 19:22:28 -0500

    data download

---
+++ Data_download.R
@@ -0,0 +1,37 @@
+### Preamble ###
+# Purpose: Download data from opendatatoronto
+# Author: Xiangyu Wang 
+# Date: 03 Feb 2022
+# Contact: xiangyu.wang@mail.utoronto.ca
+# License: Open Government Licence – Toronto
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(dplyr)
+
+# get package
+package <- show_package("cba07a90-984b-42d2-9131-701c8c2a9788")
+package
+
+# get all resources for this package
+resources <- list_package_resources("cba07a90-984b-42d2-9131-701c8c2a9788")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+death_registry <- filter(datastore_resources, row_number()==1) %>% get_resource()
+death_registry
+
+# get package
+package <- show_package("64b54586-6180-4485-83eb-81e8fae3b8fe")
+package
+
+# get all resources for this package
+resources <- list_package_resources("64b54586-6180-4485-83eb-81e8fae3b8fe")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+covid <- filter(datastore_resources, row_number()==1) %>% get_resource()
